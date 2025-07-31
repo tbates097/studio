24da44c0d8f303f9964392e3d2c4b5e5c552ef2b
Todd Bates <tbates@aerotech.com>
2025-07-31 16:21:24 +0000

    There is no way my account will have permission to deploy this.
    Can I

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 
-import { ConnectionTester } from "@/components/connection-tester";
+import { OrthoDashboard } from "@/components/ortho-dashboard";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-1 w-full p-4 mx-auto md:p-8 max-w-7xl">
-        <ConnectionTester />
+        <OrthoDashboard />
       </main>
     </div>
   );
